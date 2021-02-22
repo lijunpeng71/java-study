ff878eb00298da6f3a7341a7476c5d28a468a69d
lijunpeng71 <lijunpeng71@163.com>
2021-02-22 21:16:15 +0800

    init

---
+++ interview/src/main/java/com/study/interview/jmm/VolatileDemo.java
@@ -0,0 +1,12 @@
+package com.study.interview.jmm;
+
+/**
+ * Created on 2021/2/21.
+ *
+ * @author Administrator
+ */
+public class VolatileDemo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ spring-boot/src/main/java/com/study/springboot/SpringBootStudyApplication.java
@@ -0,0 +1,17 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created on 2021/2/21.
+ *
+ * @author Administrator
+ */
+@SpringBootApplication
+public class SpringBootStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStudyApplication.class, args);
+    }
+}
